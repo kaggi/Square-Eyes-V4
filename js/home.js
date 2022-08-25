d425cdc1b1c6dc3111a6adb2de1c2401815ae242
kaggi <lola.lohne@gmail.com>
2022-08-25 20:58:55 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,14 +1,16 @@
 let homeLink = document.querySelector("#home_link");
 homeLink.classList.add("current_page");
 
-const url = "http://squareeyes.lolalohne.com/wp-json/wc/store/products";
+const squareEyesUrl = "http://squareeyes.lolalohne.com/wp-json/wc/store/products";
 
-const displayLastUploadedFilms = document.querySelector("homepage_new_uploads");
+
+
+
 
 async function getFilms() {
     
     try {
-        const response = await fetch(url);
+        const response = await fetch(squareEyesUrl);
         const results = await response.json();
         console.log(results);
     }
@@ -17,3 +19,5 @@ async function getFilms() {
         console.log("An error occured getting films");
     }
 }
+
+getFilms();
